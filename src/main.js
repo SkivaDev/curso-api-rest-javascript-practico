14e84ebc948a1eed7931611ab7f5b568ffadb96d
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2022-10-17 17:28:23 -0500

    se agrego scroll infinito pero con un boton en la seccion trends

--- src/main.js
+++ src/main.js
@@ -21,8 +21,8 @@ const lazyLoader = new IntersectionObserver((entries) => {
 });
 
 
-function createMovies(movies, container, lazyLoad = false) {
-  container.innerHTML = '';
+function createMovies(movies, container, {lazyLoad = false, clean = true} = {}) {
+  if(clean) container.innerHTML = '';
 
   movies.forEach(movie => {
     const movieContainer = document.createElement('div');
@@ -114,8 +114,37 @@ async function getTrendingMovies() {
   const { data } = await api('trending/movie/day');
   const movies = data.results;
 
-  createMovies(movies, genericSection, true);
+  createMovies(movies, genericSection, {lazyLoad: true, clean: true});
+
+  const btnLoadMore = document.createElement('button');
+  btnLoadMore.innerText = 'Cargar más';
+  btnLoadMore.addEventListener('click', getPaginatedTrendingMovies);
+  genericSection.appendChild(btnLoadMore);
 }
+
+let page = 1;
+
+async function getPaginatedTrendingMovies() {
+  page++;
+  const { data } = await api('trending/movie/day', {
+    params: {
+      page,
+    },
+  });
+  const movies = data.results;
+
+  createMovies(
+    movies,
+    genericSection,
+    { lazyLoad: true, clean: false },
+  );
+
+  const btnLoadMore = document.createElement('button');
+  btnLoadMore.innerText = 'Cargar más';
+  btnLoadMore.addEventListener('click', getPaginatedTrendingMovies);
+  genericSection.appendChild(btnLoadMore);
+}
+
 
 async function getMovieById(id) {
   const { data: movie } = await api('movie/' + id);
